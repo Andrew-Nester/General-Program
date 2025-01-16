a261088be9fabd59ef3c0659e0a738787cdf8e0f
Nester <25NesterA@badinhs.org>
2025-01-16 13:40:47 -0500

    took notes for basic math

--- variableNotes.js
+++ variableNotes.js
@@ -1,5 +1,46 @@
 // This is a comment
 
 //declaring the variable
-let myName = "Jeff";
-myName;
+// let myName = "Jeff";
+// console.log(myName);
+// myName = "Steve";
+// console.log(myName);
+
+//Here is a different way to declare a vairbale that is constant
+// const e =2.71828;
+// console.log(e);
+//e = 3.14159;
+//console.log(e);
+// ^^^ This is illegal a constant can't change 
+
+//a third way to delcare a variable similar to let unlike const
+// var pi = 3.1415;
+// console.log(pi);
+// pi = e;
+// console.log(pi);
+
+// /*starts a multi-line or block comment
+
+//Now we start with numbers and math
+//javascript can add
+// console.log(97+23)
+// let sumOfSix = 1 + 3 + 6 + 10 + 15 + 21;
+// console.log(sumOfSix);
+// //you can store values and do math in variables or in the console
+
+// //here is a simulation of calculation % error
+// const actual = 57;
+// let prediction = actual-13;
+// let percentage = prediction/actual;
+// let precentError = 1-percentage;
+// console.log (precentError);
+
+/* Mathematical Operatiors in JS
++ add
+- subtract
+* multiply
+/ divide
+** expoentiate
+% modulus
+
+// JS will work like a  scientific calculator and apply OrderOfOperations Modulus happens after division but before addition
