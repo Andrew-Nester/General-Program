3cca48a57622af8e2e861c41277be4159fa11ccf
Nester <25NesterA@badinhs.org>
2025-01-21 13:36:56 -0500

    took more notes

--- dataTypeNotes.js
+++ dataTypeNotes.js
@@ -11,4 +11,29 @@
 3.  String  these are letters, words, characters, numbers and symbols (treated like text)
 
 */
+console.log (2+3)
+console.log ("2"+"3")
+/*
+addition using string uses concatination
+strings are formed by putting symbols in quotation marks
+
+In Java there are 3 kinds of quotes
+        The double quoation mark "Hello"
+        The Single quotationmark 'Hello'
+        THe backticks `hello`
+*/
+
+console.log ("Hello World")
+console.log ('Hello World')
+
+//If a string contains an apostrophe use double quotes, if quotation use single quotes
+//Backticks allow you to embed variables within a string
+
+let myName = "steve"
+console.log ("myName")
+console.log (`Hello, ${myName}`)
+console.log (` 2+3 ${2+3}`)
+
+//Many programming launguages have a `char` data type reperesenting a single character
+// Javascript doesn't
 
