574db901fccfea894e8da360da841bacdb393a50
Nester <25NesterA@badinhs.org>
2025-01-24 13:39:26 -0500

    took grading notes

--- conditionals.js
+++ conditionals.js
@@ -37,17 +37,42 @@ a >= b
 //inverse alphabetical order
 // longer string wins if they are toherwise the same
 
-if (3<5){
-    console.log( "3 is less than 5")
+// if (3<5){
+//     console.log( "3 is less than 5")
+// }
+
+// if (7<5){
+//     console.log(" 7 is less than 5")
+// } else {
+//     console.log ("7 isn't less than 5")
+// }
+
+// // a test for equaliy 
+// let testNumber = 2;
+
+// if(testNumber > 6){;
+//     console.log(`${testNumber} > 6`);
+// } else {;
+//     if(testNumber < 6){
+//         console.log(`${testNumber} < 6`);
+//     } else {;
+//         console.log(`${testNumber} = 6`);
+// };
+// };
+
+// better approaches
+
+// if - else if - else
+
+let testGrade = 48;
+if(testGrade >= 90){
+    console.log(" you get an A");
+} else if(testGrade >= 80){
+    console.log( "you get a B");
+} else if( testGrade >= 70){
+    console.log(" you get a C");
+} else if( testGrade >= 60){
+    console.log (" you get a D");
+} else{
+    console.log( "you get an F");
 }
-
-if (7<5){
-    console.log(" 7 is less than 5")
-} else {
-    console.log ("7 isn't less than 5")
-}
-
-// a test for equaliy 
-let testNumber = 6
-
-if (testNumber)
