3f65b90c3420c8eea63e4aa34d8401915fa8dd3e
Nester <25NesterA@badinhs.org>
2025-02-13 10:28:52 -0500

    worked on assignment

---
+++ functionPractice.js
@@ -0,0 +1,28 @@
+//1
+
+function multiply(num1, num2){
+    console.log (num1*num2)
+}
+multiply (9039,89)
+
+//6
+
+function lower(num1, num2) {
+    if (num1 > num2)
+        console.log(num2);
+    if (num2 > num1)
+        console.log(num1);
+}
+
+lower (99898, 2)
+
+//7
+
+function exponentiate(num1, num2) {
+    x = num1 ** num2
+    BigInt(x)
+    console.log(x)
+}
+
+exponentiate(89,89)
+
